b4afe510b0a8a07256aa1d6ba923eae06bd13cfb
Henry Mao <admin@calclavia.com>
2013-03-03 14:07:45 +0800

    Readded renderers for machines
    TheMike! Why did you remove it?!

--- src/minecraft/mffs/client/ClientProxy.java
+++ src/minecraft/mffs/client/ClientProxy.java
@@ -1,16 +1,20 @@
 package mffs.client;
 
 import mffs.client.renderer.RenderForceField;
+import mffs.client.renderer.RenderForcilliumExtractor;
+import mffs.client.renderer.RenderFortronCapacitor;
 import mffs.common.CommonProxy;
 import mffs.common.ModularForceFieldSystem;
+import mffs.common.tileentity.TileEntityForcilliumExtractor;
+import mffs.common.tileentity.TileEntityFortronCapacitor;
 import net.minecraft.world.World;
 import net.minecraftforge.client.MinecraftForgeClient;
 import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 
 public class ClientProxy extends CommonProxy
 {
-
 	@Override
 	public void init()
 	{
@@ -18,6 +22,8 @@ public class ClientProxy extends CommonProxy
 		MinecraftForgeClient.preloadTexture(ModularForceFieldSystem.ITEM_TEXTURE_FILE);
 		MinecraftForgeClient.preloadTexture(ModularForceFieldSystem.TEXTURE_DIRECTORY + "projector.png");
 		RenderingRegistry.registerBlockHandler(new RenderForceField());
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityFortronCapacitor.class, new RenderFortronCapacitor());
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityForcilliumExtractor.class, new RenderForcilliumExtractor());
 	}
 
 	@Override
